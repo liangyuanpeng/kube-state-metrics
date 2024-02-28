793d6ef17ec501721e6d36d4c38c3c2274a9bd3f
Lan Liang <gcslyp@gmail.com>
2024-02-28 08:25:17 +0000

    ci 1.29

--- tests/e2e.sh
+++ tests/e2e.sh
@@ -24,13 +24,13 @@ case $(uname -m) in
 esac
 
 NODE_IMAGE_NAME="docker.io/kindest/node"
-KUBERNETES_VERSION=${KUBERNETES_VERSION:-"v1.28.0"}
+KUBERNETES_VERSION=${KUBERNETES_VERSION:-"v1.29.0"}
 KUBE_STATE_METRICS_LOG_DIR=./log
 KUBE_STATE_METRICS_CURRENT_IMAGE_NAME="registry.k8s.io/kube-state-metrics/kube-state-metrics"
 KUBE_STATE_METRICS_IMAGE_NAME="registry.k8s.io/kube-state-metrics/kube-state-metrics-${ARCH}"
 E2E_SETUP_KIND=${E2E_SETUP_KIND:-}
 E2E_SETUP_KUBECTL=${E2E_SETUP_KUBECTL:-}
-KIND_VERSION=v0.20.0
+KIND_VERSION=v0.22.0
 SUDO=${SUDO:-}
 
 OS=$(uname -s | awk '{print tolower($0)}')
